99a31edd60ca2168e853a171a0c49ad126eb0e71
Eirik Klevstad <Eirik.Klevstad@gmail.com>
2015-04-08 18:31:22 -0700

    asd

--- src/tasks/TaskEuclideanTsp.java
+++ src/tasks/TaskEuclideanTsp.java
@@ -3,15 +3,39 @@ package tasks;
 import api.Task;
 
 public class TaskEuclideanTsp implements Task {
+	
+	private double[][] _cities;
 
 	public TaskEuclideanTsp(double[][] cities) {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public Object execute() {
-		// TODO Auto-generated method stub
-		return null;
+	public int[] execute() {
+		double[][] cities = _cities;
+		
+		
+		for (int i = 0; i < cities.length; i++)
+		{
+			for (int j = 0; j < cities[0].length; j++)
+			{
+				
+			}
+		}
+		
+		double start_x = 0;
+		double start_y = 0;
+		
+		int[] tour = new int[cities.length];
+		
+		
+		
+		return tour;
+	}
+	
+	private int getDistanceBetweenCities(int origin_x, int origin_y, int destination_x, int destination_y)
+	{
+		return (int) Math.sqrt(Math.pow(origin_x - destination_x, 2) + Math.pow(origin_y - destination_y, 2));
 	}
 
 }
